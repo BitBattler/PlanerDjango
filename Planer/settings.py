0e7aaacb5013bf8764789ede016ea8fd16ee1b3e
BitBattler <159486116+BitBattler@users.noreply.github.com>
2024-05-02 07:25:39 +0200

    Allowed Host settings.py

--- Planer/settings.py
+++ Planer/settings.py
@@ -157,10 +157,10 @@ DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': lambda request: DEBUG,
 }
 # Django CSRF-Schutz: CSRF_TRUSTED_ORIGINS
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'http://localhost', 'http://node180a.containers.back4app.com', 'https://planer4-e93a8s4x.b4a.run']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'http://localhost', 'http://node84a.containers.back4app.com', 'https://planer4-e93a8s4x.b4a.run']
 
 # CORS-Konfiguration: CORS_ALLOWED_ORIGINS
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1', 'http://localhost', 'http://node180a.containers.back4app.com', 'https://planer4-e93a8s4x.b4a.run']
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1', 'http://localhost', 'http://node84a.containers.back4app.com', 'https://planer4-e93a8s4x.b4a.run']
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
